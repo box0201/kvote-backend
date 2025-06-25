ae163eab0f69d35adeb48893ab01b2334b905365
box0201 <box0201@gmail.com>
2025-06-25 09:44:57 +0000

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import re
 import os
 from glob import glob
 
@@ -9,12 +10,13 @@ st.title("📊 Pregled kvota")
 folder_path = "csv"  # Tvoj folder sa CSV fajlovima
 csv_files = glob(os.path.join(folder_path, "*.csv"))
 
-for file in csv_files:
-    df = pd.read_csv(file)
+for file_path in csv_files:
+    file_name = os.path.basename(file_path)  # npr. 'utakmica_123_47.csv'
+    match = re.search(r'_(\d+)\.csv$', file_name)
+    procenat = match.group(1) if match else "?" 
+    df = pd.read_csv(file_path)
     df_new = df.drop(columns=['vreme', 'domaci', 'gosti']).reset_index(drop=True)
-    
-    # Naslov za expander (sklopivi blok)
-    title = f"⚽ {df.iloc[0]['domaci']} vs {df.iloc[0]['gosti']}  —  🕒 {df.iloc[0]['vreme']}"
+    title = f"⚽ {df.iloc[0]['domaci']} vs {df.iloc[0]['gosti']}  —  🕒 {df.iloc[0]['vreme']}  —  {procenat}"
 
     with st.expander(title):
         st.dataframe(df_new)
