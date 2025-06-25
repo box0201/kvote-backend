908ae09c12f860d5c217ef9f8fb1c5280c45f75f
box0201 <box0201@gmail.com>
2025-06-25 10:36:28 +0000

    func

--- func.py
+++ func.py
@@ -3,8 +3,14 @@ def highlight_max_except_id(df):
         is_max = s == s.max()
         return ['background-color: lightgreen' if v else '' for v in is_max]
 
-    # Primenjuj funkciju samo na kolone osim 'ID'
-    return df.style.apply(
-      lambda col: highlight_max(col) if col.name != 'ID' else ['' for _ in col],
-      axis=0
-      ).format("{:.2f}")
+    styler = df.style.apply(
+        lambda col: highlight_max(col) if col.name != 'ID' else ['' for _ in col],
+        axis=0
+    )
+    
+    # Formatiraj samo float kolone sa dve decimale
+    float_cols = df.select_dtypes(include=['float', 'float64']).columns
+    if len(float_cols) > 0:
+        styler = styler.format({col: "{:.2f}" for col in float_cols})
+
+    return styler
