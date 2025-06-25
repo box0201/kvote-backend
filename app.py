71dea2e6f86fe659f416c1cfea5429a9442afa34
box0201 <box0201@gmail.com>
2025-06-25 09:38:15 +0000

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ csv_files = glob(os.path.join(folder_path, "*.csv"))
 for file in csv_files:
     df = pd.read_csv(file)
     df_new = df.drop(columns=['vreme', 'domaci', 'gosti']).reset_index(drop=True)
-    st.markdown(f"## ⚽ {df.iloc[0]['domaci']} vs {df.iloc[0]['gosti']}")
-    st.markdown(f"🕒 {df.iloc[0]['vreme']}")
-    st.dataframe(df_new)
-    st.markdown("---")
+    
+    # Naslov za expander (sklopivi blok)
+    title = f"⚽ {df.iloc[0]['domaci']} vs {df.iloc[0]['gosti']}  —  🕒 {df.iloc[0]['vreme']}"
+
+    with st.expander(title):
+        st.dataframe(df_new)
