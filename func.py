df4b95f982cfb0848def37da23adb3828801f98e
box0201 <box0201@gmail.com>
2025-06-25 10:41:48 +0000

    func

--- func.py
+++ func.py
@@ -1,5 +1,6 @@
+import pandas as p 
+
 def highlight_max_except_id(df):
-    # Zaokruži sve numeričke kolone osim 'ID' na 2 decimale
     df_rounded = df.copy()
     for col in df_rounded.columns:
         if col != 'ID' and pd.api.types.is_numeric_dtype(df_rounded[col]):
@@ -9,7 +10,6 @@ def highlight_max_except_id(df):
         is_max = s == s.max()
         return ['background-color: lightgreen' if v else '' for v in is_max]
 
-    # Primeni stil na zaokruženi DataFrame
     return df_rounded.style.apply(
         lambda col: highlight_max(col) if col.name != 'ID' else ['' for _ in col],
         axis=0
