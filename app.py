2c8476c32b233f5d8d5527f6833257da3df598d1
box0201 <box0201@gmail.com>
2025-06-25 09:11:15 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import os
+from glob import glob
+
+st.set_page_config(page_title="Kvote", layout="wide")
+st.title("📊 Pregled kvota")
+
+folder_path = "/content/drive/MyDrive/BOT/sajt"  # Tvoj folder sa CSV fajlovima
+csv_files = glob(os.path.join(folder_path, "*.csv"))
+
+for file in csv_files:
+    df = pd.read_csv(file)
+    df_new = df.drop(columns=['vreme', 'domaci', 'gosti']).reset_index(drop=True)
+    st.markdown(f"## ⚽ {df.iloc[0]['domaci']} vs {df.iloc[0]['gosti']}")
+    st.markdown(f"🕒 {df.iloc[0]['vreme']}")
+    st.dataframe(df_new)
+    st.markdown("---")
