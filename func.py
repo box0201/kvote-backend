9e2ecf00f14ee98f1a7a2eaed2d65ac87b3928a9
box0201 <box0201@gmail.com>
2025-06-25 11:08:05 +0000

    func

--- func.py
+++ func.py
@@ -2,7 +2,7 @@ def highlight_max_except_id(df):
 
     def highlight_max(s):
         is_max = s == s.max()
-        return ['background-color: lightgreen' if v else '' for v in is_max]
+        return ['background-color: lightblue' if v else '' for v in is_max]
 
     styler = df.style.apply(
         lambda col: highlight_max(col) if col.name != 'ID' else ['' for _ in col],
