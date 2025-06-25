68260b3e5bd1b867a4ecd9ee33fb82d7c619c435
box0201 <box0201@gmail.com>
2025-06-25 10:39:24 +0000

    func

--- func.py
+++ func.py
@@ -1,8 +1,10 @@
-import pandas as pd
-
-def highlight_max_per_column(df, columns):
+def highlight_max_except_id(df):
     def highlight_max(s):
         is_max = s == s.max()
         return ['background-color: lightgreen' if v else '' for v in is_max]
 
-    return df.style.apply(lambda col: highlight_max(col) if col.name in columns else ['' for _ in col], axis=0)
+    # Primenjuj funkciju samo na kolone osim 'ID'
+    return df.style.apply(
+        lambda col: highlight_max(col) if col.name != 'ID' else ['' for _ in col],
+        axis=0
+    )
